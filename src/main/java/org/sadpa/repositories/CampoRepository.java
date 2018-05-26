265082ad93583367aef4c8daa154ea25c7e2279a
Elton Gonçalves <elton.private@gmail.com>
2018-05-26 00:59:12 -0300

    atualização

--- src/main/java/org/sadpa/repositories/CampoRepository.java
+++ src/main/java/org/sadpa/repositories/CampoRepository.java
@@ -13,7 +13,7 @@ public interface CampoRepository extends JpaRepository<Campo, String> {
 	
 	  List<Campo> findByCamada(Camada camada);
 	  
-	  @Query("select c from Campo c where c.camada = :camada and  c.situacao between 0 and :situacao")
+	  @Query("select c from Campo c where c.camada = :camada and  c.situacao between 0 and :situacao order by c.nome")
 	  List<Campo> findByCamadaAndSituacao(
 			  @Param("camada") Camada camada,
               @Param("situacao") int situacao
